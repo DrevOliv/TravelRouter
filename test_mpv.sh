b1ad75adb3bf7a8029a4d4745680acb741eaca34
Oliver Drevet <oliver.drevet@outlook.com>
2026-03-17 13:18:31 +0100

    commands

--- test_mpv.sh
+++ test_mpv.sh
@@ -1,1 +1,3 @@
 mpv --fs --hwdec=auto --input-ipc-server=/tmp/pi-travel-router-mpv.sock --idle=yes --start=20 http://192.168.0.52/Videos/276d92972aec1bf7a143191bbe3c527b/stream?static=true&api_key=56a4e3689ab14822ab2e72a312e4d2bc
+
+mpv --vo=drm --hwdec=v4l2m2m-copy --drm-connector=1
